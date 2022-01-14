955ce4d8a1d77400ffbf8e4e9f5624ac2e21c83c
ddmoyu <daydaymoyu@gmail.om>
2022-01-14 20:07:07 +0800

    feat: start add command dev

--- src/commands/add/index.ts
+++ src/commands/add/index.ts
@@ -1,9 +1,21 @@
 import { Command } from '@oclif/core'
+import { ArgInput } from '@oclif/core/lib/interfaces'
+import chalk = require('chalk')
 
 export default class Add extends Command {
   static description = 'add git user'
 
+  static args: ArgInput = [{ name: 'name' }, { name: 'email' }]
+
   async run(): Promise<void> {
-    console.log('== add ==')
+    const { args } = await this.parse(Add)
+    const name = args.name
+    const email = args.email
+
+    if (!name || !email) {
+      console.log(chalk.yellow('miss name or email \ngu add name email'))
+    }
+
+    console.log('== add ==', name, email)
   }
 }
