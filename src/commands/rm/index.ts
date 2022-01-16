59ec03fbeccac6adc2bc36fea0a736bf37fd7eb2
ddmoyu <daydaymoyu@gmail.om>
2022-01-16 19:00:49 +0800

    feat: delete git user function

--- src/commands/rm/index.ts
+++ src/commands/rm/index.ts
@@ -1,9 +1,15 @@
 import { Command } from '@oclif/core'
+import { ArgInput } from '@oclif/core/lib/interfaces'
+import db from '../../utils/db'
 
 export default class Rm extends Command {
   static description = 'remove git user'
+  static args: ArgInput = [{ name: 'id_name' }]
 
   async run(): Promise<void> {
-    console.log('== rm ==')
+    const { args } = await this.parse(Rm)
+    const idName = args.id_name
+    const flag = db.delete(idName)
+    console.log('== rm flag ==', flag)
   }
 }
