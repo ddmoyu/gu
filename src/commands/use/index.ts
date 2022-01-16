2b80c2355f968c5bd94aeecff7f412804c17c528
ddmoyu <daydaymoyu@gmail.com>
2022-01-16 19:56:43 +0800

    feat: get git user

--- src/commands/use/index.ts
+++ src/commands/use/index.ts
@@ -1,9 +1,14 @@
 import { Command } from '@oclif/core'
+import { getLocal, getGlobal } from '../../utils/git'
 
 export default class Use extends Command {
   static description = 'use git user'
 
   async run(): Promise<void> {
-    console.log('== use ==')
+    // console.log('== use ==')
+    const res = getLocal()
+    console.log('=== local ===', res)
+    const re = getGlobal()
+    console.log('=== global ===', re)
   }
 }
---
+++ src/utils/git.ts
@@ -0,0 +1,32 @@
+import shell = require('shelljs')
+
+interface User { [key: string]: string }
+
+function getLocal(): User {
+  const name = shell.exec('git config --local user.name').stdout
+  if (name) {
+    const email = shell.exec('git config --local user.email').stdout
+    if (email) {
+      return { name: name.replace('\n', ''), email: email.replace('\n', '') }
+    }
+  }
+
+  return { name: '', email: '' }
+}
+
+function getGlobal(): User {
+  const name = shell.exec('git config --global user.name').stdout
+  if (name) {
+    const email = shell.exec('git config --global user.email').stdout
+    if (email) {
+      return { name: name.replace('\n', ''), email: email.replace('\n', '') }
+    }
+  }
+
+  return { name: '', email: '' }
+}
+
+export {
+  getLocal,
+  getGlobal
+}
